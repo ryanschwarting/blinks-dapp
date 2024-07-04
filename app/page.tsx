108869e1cd187efb3d03a0ff597170c02ad59280
ryanschwarting <schwartingryan@yahoo.com>
2024-07-04 16:18:15 -0600

    added code to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,51 @@
 import { NavBar } from "../components/NavBar";
+import {
+  CoinsIcon,
+  FileTextIcon,
+  ImageIcon,
+  ShieldIcon,
+  WalletIcon,
+} from "lucide-react";
+
+const actionCards: Array<{
+  title: string;
+  href: string;
+  description: React.ReactNode;
+  icon: React.ReactNode;
+}> = [
+  {
+    title: "On-chain Memo",
+    href: "/memo",
+    description: "Send a simple message on-chain using an SPL Memo.",
+    icon: <FileTextIcon className="size-12" />,
+  },
+  {
+    title: "Staking SOL",
+    href: "/stake",
+    description:
+      "Help secure the Solana network by staking SOL to a validator.",
+    icon: <ShieldIcon className="size-12" />,
+  },
+  {
+    title: "Transfer Native SOL",
+    href: "/transfer-sol",
+    description: "Easily transfer native SOL to any other Solana wallet.",
+    icon: <WalletIcon className="size-12" />,
+  },
+  // {
+  //   title: "Transfer SPL Tokens",
+  //   href: "/transfer-spl",
+  //   description: "Easily transfer SPL tokens to any other Solana wallet.",
+  //   icon: <CoinsIcon className="size-12" />,
+  // },
+  // {
+  //   title: "Mint an NFT",
+  //   href: "/mint-nft",
+  //   description:
+  //     "Allow anyone to claim a digital collectible from a Collection.",
+  //   icon: <ImageIcon className="size-12" />,
+  // },
+];
 
 export default function HomePage() {
   return (
