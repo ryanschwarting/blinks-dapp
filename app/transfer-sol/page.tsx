32dac4ef2511e1f41e5274790b593e0761eab284
ryanschwarting <schwartingryan@yahoo.com>
2024-07-05 12:43:05 -0600

    tailwind changes for mobile UI

--- app/transfer-sol/page.tsx
+++ app/transfer-sol/page.tsx
@@ -21,22 +21,31 @@ export default function Pages() {
   }, []);
 
   return (
-    <section id="action" className="space-y-8 p-8 px-20">
-      <div className="flex justify-between items-center h-[100px]">
-        <Image src={solanaLogo} width={300} height={300} alt="SolanaLogo" />
+    <section
+      id="action"
+      className="space-y-2 md:space-y-8 p-2 md:p-8 px-4 md:px-20"
+    >
+      <div className="flex justify-between items-center h-[50px] md:h-[100px]">
+        <Image
+          src={solanaLogo}
+          width={300}
+          height={300}
+          alt="SolanaLogo"
+          className="w-[200px] h-[30px] md:w-[300px] md:h-[50px]"
+        />
         <Image
           src={kaktos}
           width={100}
           height={100}
           alt="SMB"
-          className="rounded-full"
+          className="rounded-full h-[45px] w-[45px] md:w-[100px] md:h-[100px]"
         />
       </div>
-      <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-6 text-center">
+      <div className="mx-auto flex w-full md:max-w-[58rem] flex-col items-center space-y-6 text-center pt-4 md:pt-0">
         <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl text-gray-300">
           Donate Solana to Kaktos
         </h2>
-        <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7 text-gray-300">
+        <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7 text-gray-300 pb-4 md:pb-0">
           I am embarking on an exciting journey, transitioning from an
           experienced Ethereum developer to diving deep into the Solana
           development ecosystem. Your generous support will empower me to
@@ -47,17 +56,27 @@ export default function Pages() {
         </p>
       </div>
 
-      <div className="group-hover:border-primary size-[400px] rounded overflow-hidden text-center flex items-center justify-center w-min mx-auto border border-gray-300 shadow-lg">
+      <div className="group-hover:border-primary size-[400px] rounded-lg overflow-hidden text-center hidden md:flex items-center justify-center w-min mx-auto border-4 border-[#14F195] shadow-lg">
         <SolanaQRCode
           url={apiPath}
           color="#14F195"
           background="#9945FF"
           size={400}
-          className="rounded-lg overflow-clip min-w-[400px]"
+          className="rounded-lg overflow-clip w-[400px]"
         />
       </div>
 
-      <div className="mx-auto text-center md:max-w-[58rem]">
+      <div className="group-hover:border-primary size-[200px] md:size-[400px] rounded-lg overflow-hidden text-center flex md:hidden items-center justify-center w-min mx-auto border-4 border-[#14F195] shadow-lg">
+        <SolanaQRCode
+          url={apiPath}
+          color="#14F195"
+          background="#9945FF"
+          size={200}
+          className="rounded-lg overflow-clip min-w-[200px] md:min-w-[400px]"
+        />
+      </div>
+
+      <div className="mx-auto text-center max-w-[85%] pt-4 md:pt-0">
         <p className="text-gray-300 leading-normal text-muted-foreground sm:text-lg sm:leading-7">
           View the{" "}
           <Link
@@ -78,7 +97,7 @@ export default function Pages() {
             Action Endpoint
           </h3>
         </div>
-        <div className="pt-4 space-y-2">
+        <div className="pt-4 space-y-2 text-[14px] md:text-lg">
           <p className="text-muted-foreground">
             <Link
               href={apiEndpoint}
